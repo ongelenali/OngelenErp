460250018399e4c7bb0c469f3a060aacfa1d5ffb
Ali <Ali@DESKTOP-DHPL4S9>
2023-01-30 15:30:13 +0300

    Proje dosyası ekle.

---
+++ Arch.Core/Constants/StaticParams.cs
@@ -0,0 +1,9 @@
+﻿namespace Arch.Core.Constants
+{
+    public struct StaticParams
+    {
+        public const string Dashboard = "/Dashboard";
+        public const string SifrelemeParametresi = "testfalan";
+        public const string DefaultConnstr = "Data Source=DESKTOP-DHPL4S9\\SQLEXPRESS;initial Catalog=WorkDB;user id=sa;password=123456;MultipleActiveResultSets=True";
+    }
+}
